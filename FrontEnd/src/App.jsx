82b77e69d6b17e4edddbfbffcfed12223905cd00
Karan <kc2532002@gmail.com>
2024-12-18 10:56:51 +0530

    checkin

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,12 +5,14 @@ import Navbar from "./component/Navbar";
 import SuccessPage from "../src/component/SuccessPage";
 import ErrorPage from "../src/component/ErrorPage";
 import CheckInPage from "./component/CheckInPage";
+import CheckInVisitor from "./component/CheckInVisitor";
 
 function App() {
   return (
     <div>
       <>
-        <CheckInPage/>
+        <CheckInVisitor/>
+        {/* <CheckInPage/> */}
         {/* <SuccessPage/>*/}
         {/*<ErrorPage/> */}
         {/* <Dashboard/> */}
--- FrontEnd/src/component/CheckInPage.jsx
+++ FrontEnd/src/component/CheckInPage.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import VisionEdge from "../utils/VisionEdge";
-// import CustomWebcam from "../utils/CustomWebCam";
-
-// const CheckInPage = () => {
-//   return (
-//     <div>
-//       <VisionEdge />
-
-//       <div className="flex flex-col items-center">
-//         <p className="text-gray-600 text-2xl sm:text-3xl md:text-4xl mt-6 mb-8 max-w-xl text-center font-thin">
-//           Smile for the Photograph
-//         </p>
-
-//         {/* Webcam Component */}
-//         <div className="relative w-full max-w-screen-lg flex justify-center items-center bg-white shadow-md rounded-lg p-4">
-//           <CustomWebcam />
-//         </div>
-
-//         <button
-//           type="button"
-//           className="mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-6 py-3 transition-all duration-200 ease-in-out"
-//         >
-//           Capture
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CheckInPage;
-
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import VisionEdge from "../utils/VisionEdge";
